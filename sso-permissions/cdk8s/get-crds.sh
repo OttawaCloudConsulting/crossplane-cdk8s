063dae1f8ccc09d49e58cbf1f7229d6a06387083
christian turner <cturner@ottawacloudconsulting.com>
2024-09-25 09:44:00 -0400

    init commit with updated get-crds.sh

---
+++ sso-permissions/cdk8s/get-crds.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Array of keywords to filter CRDs.
+provider_package=(
+  "identitystore.aws.upbound.io"
+  "ssoadmin.aws.upbound.io"
+  )
+
+# Create the directory if it doesn't exist and initialize the output YAML file.
+mkdir -p crds
+: > crds/crossplane.yaml
+
+# Loop through the array and filter CRDs for each keyword.
+for keyword in "${provider_package[@]}"; do
+  # Get the list of CRDs that match the aws.upbound.io domain and the current keyword.
+  while IFS= read -r crd; do
+    echo "---" >> crds/crossplane.yaml
+    kubectl get crd "$crd" --output yaml >> crds/crossplane.yaml
+  done < <(kubectl get crd --no-headers | grep aws.upbound.io | grep "$keyword" | awk '{print $1}')
+done
